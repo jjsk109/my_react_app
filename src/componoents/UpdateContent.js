c611dbd8d6629cd391f515987b8c315bc236e5a2
jjsk109 <jjsk109@gmail.com>
2021-07-11 23:24:37 +0900

    update update

--- src/componoents/UpdateContent.js
+++ src/componoents/UpdateContent.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 
 class UpdateContent extends Component{
-    shouldComponentUpdate(){
-        
+    constructor(props){
+        super(props);
+        this.state = {
+            title:this.props.data.title,
+            desc:this.props.data.desc,
+        }
+        this.inputFormHandler = this.inputFormHandler.bind(this);
+    }
+    inputFormHandler(e){
+        this.setState({[e.target.name]:e.target.value});
     }
     render(){
+    
         console.log("UpdateContent render");
         console.log(this.props.data);
         return(
@@ -20,9 +29,19 @@ class UpdateContent extends Component{
                         )
                     }.bind(this)}
                >
-                   <p><input type="text" name="title" placeholder="title" value={this.props.data.title} ></input></p>
+                   <p><input type="text" 
+                        name="title" 
+                        placeholder="title" 
+                        value={this.state.title} 
+                        onChange ={this.inputFormHandler}
+                        ></input></p>
                    <p>
-                       <textarea name="desc" placeholder="description"value={this.props.data.desc}></textarea>
+                       <textarea 
+                            name="desc" 
+                            placeholder="description"
+                            value={this.state.desc}
+                            onChange ={this.inputFormHandler}
+                        ></textarea>
                    </p>
                    <p>
                        <input type="submit"></input>
