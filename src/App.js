d9c83da6f57ea3fb3752dae92622dc688a84523e
jjsk109 <jjsk109@gmail.com>
2021-07-12 17:52:25 +0900

    delete add

--- src/App.js
+++ src/App.js
@@ -52,30 +52,43 @@ class App extends Component {
         // add content to this.state.contents
         console.log(_title,_desc);
         this.max_content_id = this.max_content_id + 1;
+
+        // 1. push를 사용하여 원본 변경
         // this.state.contents.push(
         //   {id:this.max_content_id, title:_title,desc:_desc}
         // );
+
+        // 2. concat을 사용하여 원본 보존
         // var _cibtebts = this.state.contents.concat(
         //   {id:this.max_content_id, title:_title, desc:_desc}
         // )
+        
         var newContents = Array.from(this.state.contents);
         newContents.push({id:this.max_content_id,title:_title,desc:_desc});
         this.setState({
-          contents:newContents
+          contents:newContents,
+          mode:'read',
+          selected_content_id:this.max_content_id
         });
       }.bind(this)} />;
     }else if(this.state.mode === 'update'){
       var _content = this.getReadContent();
 
-      _article =  <UpdateContent data = {_content} onSubmit={function(_title,_desc){
-        
-        console.log(_title,_desc);
-        this.max_content_id = this.max_content_id + 1;
-       
-        var newContents = Array.from(this.state.contents);
-        newContents.push({id:this.max_content_id,title:_title,desc:_desc});
+      _article =  <UpdateContent data = {_content} onSubmit={function(_id,_title,_desc){
+        var _contents = Array.from(this.state.contents);
+        var i = 0;
+        console.log(_id,_title,_desc);
+        while(i < _contents.length){
+          if(_contents[i].id === _id){
+            _contents[i] = {id:_id,title:_title,desc:_desc};
+            break;
+          }
+          i = i +1;
+        }
+      
         this.setState({
-          contents:newContents
+          contents:_contents,
+          mode:'read'
         });
       }.bind(this)} />;
     }
@@ -105,9 +118,28 @@ class App extends Component {
         data={this.state.contents}/>
       <Control
         onChangePage={function(_mode){
-          this.setState({
-            mode:_mode
-          });
+          if(_mode === 'delete'){
+            if(window.confirm('really?')){
+              var _contents = Array.from(this.state.contents);
+              var i = 0;
+              while(i < this.state.contents.length){
+                if(_contents[i].id === this.state.selected_content_id){
+                  _contents.splice(i,1);
+                  break;
+                }
+                i = i + 1;
+              }
+              this.setState({
+                mode:'welcome',
+                contents:_contents
+              });
+              alert('deleted');
+            }
+          }else{
+            this.setState({
+              mode:_mode
+            });
+          }
         }.bind(this)}
       />
       {this.getContent()}
--- src/componoents/UpdateContent.js
+++ src/componoents/UpdateContent.js
@@ -4,6 +4,7 @@ class UpdateContent extends Component{
     constructor(props){
         super(props);
         this.state = {
+            id:this.props.data.id,
             title:this.props.data.title,
             desc:this.props.data.desc,
         }
@@ -15,37 +16,38 @@ class UpdateContent extends Component{
     render(){
     
         console.log("UpdateContent render");
-        console.log(this.props.data);
         return(
            <article>
-               <h2>Update</h2>
-               <form action="/create_pocess" method="post"
+                <h2>Update</h2>
+                <form action="/create_pocess" method="post"
                     onSubmit={function(e){
                         e.preventDefault()
-                        
+                        console.log("submit",this.state.id,this.state.title,this.state.desc)
                         this.props.onSubmit(
-                            e.target.title.value,
-                            e.target.desc.value
+                            this.state.id,
+                            this.state.title,
+                            this.state.desc
                         )
                     }.bind(this)}
-               >
-                   <p><input type="text" 
-                        name="title" 
-                        placeholder="title" 
-                        value={this.state.title} 
-                        onChange ={this.inputFormHandler}
-                        ></input></p>
-                   <p>
-                       <textarea 
-                            name="desc" 
-                            placeholder="description"
-                            value={this.state.desc}
+                >    
+                    <input type="hidden" name="id" value={this.state.id}></input>
+                    <p><input type="text" 
+                            name="title" 
+                            placeholder="title" 
+                            value={this.state.title} 
                             onChange ={this.inputFormHandler}
-                        ></textarea>
-                   </p>
-                   <p>
-                       <input type="submit"></input>
-                   </p>
+                            ></input></p>
+                    <p>
+                        <textarea 
+                                name="desc" 
+                                placeholder="description"
+                                value={this.state.desc}
+                                onChange ={this.inputFormHandler}
+                            ></textarea>
+                    </p>
+                    <p>
+                        <input type="submit"></input>
+                    </p>
                </form>
            </article>
         )
